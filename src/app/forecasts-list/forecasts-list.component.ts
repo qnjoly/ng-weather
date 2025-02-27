ab3328f6d3cf2d0a801739269b665eebd4c884aa
Quentin JOLY <quentin.joly@soprasteria.com>
2025-02-27 18:03:03 +0100

    feat: ajout prettier

--- src/app/forecasts-list/forecasts-list.component.ts
+++ src/app/forecasts-list/forecasts-list.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
-import {WeatherService} from '../weather.service';
-import {ActivatedRoute} from '@angular/router';
-import {Forecast} from './forecast.type';
+import { ActivatedRoute } from '@angular/router';
+import { WeatherService } from '../weather.service';
+import { Forecast } from './forecast.type';
 
 @Component({
-    selector: 'app-forecasts-list',
-    templateUrl: './forecasts-list.component.html',
-    styleUrls: ['./forecasts-list.component.css'],
-    standalone: false
+  selector: 'app-forecasts-list',
+  templateUrl: './forecasts-list.component.html',
+  styleUrls: ['./forecasts-list.component.css'],
+  standalone: false,
 })
 export class ForecastsListComponent {
-
   zipcode: string;
   forecast: Forecast;
 
-  constructor(protected weatherService: WeatherService, route : ActivatedRoute) {
-    route.params.subscribe(params => {
+  constructor(protected weatherService: WeatherService, route: ActivatedRoute) {
+    route.params.subscribe((params) => {
       this.zipcode = params['zipcode'];
-      weatherService.getForecast(this.zipcode)
-        .subscribe(data => this.forecast = data);
+      weatherService.getForecast(this.zipcode).subscribe((data) => (this.forecast = data));
     });
   }
 }
